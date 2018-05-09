89a61f3c4f6d55d1a68b30ac096682483525ba93
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2018-05-09 18:40:32 +0900

    ...

--- src/main/scala/com/ru/waka/Hello.scala
+++ src/main/scala/com/ru/waka/Hello.scala
@@ -1,9 +1,26 @@
 package com.ru.waka
 
+import scalikejdbc.{DBSession, NamedDB, SQL}
+
+import scala.util.control.Exception._
+
 object Hello {
   def main(args: Array[String]): Unit = {
 
   }
 }
 
-class HelloRepository()
+class HelloRepository(connectionName: Symbol) {
+  def put(hello: String): Either[Throwable, Unit] = catching(classOf[Throwable]) either {
+    val q =
+      """
+        |INSERT INTO hoge (foo) VALUES (?);
+      """.stripMargin
+    NamedDB(connectionName) localTx { implicit session: DBSession =>
+      SQL(q).bind()
+
+
+    }
+
+  }
+}
